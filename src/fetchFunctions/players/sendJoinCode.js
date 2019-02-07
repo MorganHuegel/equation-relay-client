86882c8f41e1ca9340984d7527f06c04584473a8
MorganHuegel <mhue2910@gmail.com>
2019-02-07 13:18:37 -0500

    added fetch function for checking game code

--- src/fetchFunctions/players/sendJoinCode.js
+++ src/fetchFunctions/players/sendJoinCode.js
@@ -2,5 +2,30 @@ import { SERVER_BASE_URL } from '../../config';
 import { io } from 'socket.io-client';
 
 export function sendJoinCode(joinCode) {
-  
+  return fetch(`${SERVER_BASE_URL}/checkGameSession/${joinCode}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => {
+      if (res.status !== 200) {
+        return res.json().then(err => Promise.reject(err));
+      }
+      return res.json();
+    })
+    .then(sessionData => {
+      if (sessionData.error) {
+        return Promise.reject(sessionData);
+      }
+      console.log('SESSION DATA:', sessionData)
+    })
+    .catch(err => {
+      console.log('IN FETCH',err)
+      if (err.message) {
+        return Promise.reject(err)
+      } else {
+        return Promise.reject({message: 'System error. Please try again.'})
+      }
+    })
 }
--- src/landingPage/joinLanding/joinLanding.js
+++ src/landingPage/joinLanding/joinLanding.js
@@ -1,20 +1,30 @@
 import React from 'react';
 
+import { sendJoinCode } from '../../fetchFunctions/players/sendJoinCode';
+
 export class JoinLanding extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      submitted: false
+      submitted: false,
+      error: null
     }
   }
 
   onSubmit = (e) => {
     e.preventDefault();
     const joinCode = document.getElementById('join-code').value;
+    return sendJoinCode(joinCode)
+      .then(sessionData => {
+
+      })
+      .catch(err => {
+        console.log('ErR',err)
+        this.setState({error: err.message})
+      })
     // Make query request to server to make sure this join code exists
       //if it does not exist, show error message
     // If it exists, redirect to the /sessionCode url component joinMain
-    return ;
   }
 
   render(){
@@ -26,6 +36,9 @@ export class JoinLanding extends React.Component {
 
           <button type='submit'>JOIN</button>
         </form>
+        <p>
+          {this.state.error}
+        </p>
       </div>
     );
   }
