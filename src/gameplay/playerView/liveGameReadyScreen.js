93af0e6e4477191a27f7d5fa5fe9a2ed6cce11fd
MorganHuegel <mhue2910@gmail.com>
2019-02-21 14:11:45 -0500

    updated Player Ready component

--- src/gameplay/playerView/liveGameReadyScreen.js
+++ src/gameplay/playerView/liveGameReadyScreen.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export function LiveGameReadyScreen (props) {
-  const teamName = props.currentTeam ? props.currentTeam.teamName : '';
-  
+export function LiveGameReadyScreen (props) { 
+  const teamList = props.currentTeam.players.map( (player, index) => {
+    return (
+      <ol key={player._id}>
+        <li>Player {index + 1} - {player.handle}</li>
+      </ol>
+    )
+  })
   return (
-    <p>Live Game Here! {teamName}</p>
+    <div>
+      {teamList}
+      <p>Current Score: {props.currentTeam.points}</p>
+      <p>{props.currentTeam.teamName}, ready your pencils!</p>
+    </div>
   )
 }
