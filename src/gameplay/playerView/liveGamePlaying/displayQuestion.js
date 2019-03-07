8d61b3b81e23cd36332fee3b9a19cf350efbe766
MorganHuegel <mhue2910@gmail.com>
2019-03-07 14:15:16 -0500

    updated styles on sovling screen

--- src/gameplay/playerView/liveGamePlaying/displayQuestion.js
+++ src/gameplay/playerView/liveGamePlaying/displayQuestion.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import '../../../stylesheets/gameplay/playerView/liveGamePlaying/displayQuestion.css';
+
 export function DisplayQuestion (props) {
   if (props.currentUser.alreadyGuessed && props.currentUser.correctGuess) {
     return (
-      <div>
-        <p>Solve your equation:</p>
-        <p>{props.equation}</p>
-        <p>Then add the solutions of all {props.numOfTeammates} group members:</p>
+      <div className='display-question'>
+        <p className='instructions'>Solve your equation:</p>
+        <p className='display-equation'>{props.equation}</p>
+        <p className='instructions'>Then <span>add the solutions</span> of all {props.numOfTeammates} group members:</p>
         <p>Correct submission! Tell yo' friends!</p>
       </div>
     );
@@ -14,10 +16,10 @@ export function DisplayQuestion (props) {
 
   else if (props.currentUser.alreadyGuessed) {
     return (
-      <div>
-        <p>Solve your equation:</p>
-        <p>{props.equation}</p>
-        <p>Then add the solutions of all {props.numOfTeammates} group members:</p>
+      <div className='display-question'>
+        <p className='instructions'>Solve your equation:</p>
+        <p className='display-equation'>{props.equation}</p>
+        <p className='instructions'>Then <span>add the solutions</span> of all {props.numOfTeammates} group members:</p>
         <p>Nope, sorry! Did you add the answers of all your teammates?</p>
       </div>
     )
@@ -25,10 +27,10 @@ export function DisplayQuestion (props) {
 
   else {
     return (
-      <div>
-        <p>Solve your equation:</p>
-        <p>{props.equation}</p>
-        <p>Then add the solutions of all {props.numOfTeammates} group members:</p>
+      <div className='display-question'>
+        <p className='instructions'>Solve your equation:</p>
+        <p className='display-equation'>{props.equation}</p>
+        <p className='instructions'>Then <span>add the solutions</span> of all {props.numOfTeammates} group members:</p>
         <label htmlFor='group-solution'>Total is...</label>
         <input type='text' name='group-solution' id='group-solution'/>
         <button type='button' onClick={props.checkAnswer}>Submit</button>
--- src/gameplay/playerView/liveGamePlaying/headerSubmissionList.js
+++ src/gameplay/playerView/liveGamePlaying/headerSubmissionList.js
@@ -1,20 +1,23 @@
 import React from 'react';
 
+import '../../../stylesheets/gameplay/playerView/liveGamePlaying/headerSubmissionList.css';
+
 export function HeaderSubmissionList (props) {
   const playersList = props.teamData.players.map(player => {
     const key = player._id;
     if (player.alreadyGuessed) {
-      return <li key={key}>{player.handle}</li>
+      const correct = player.correctGuess ? 'correct' : 'wrong';
+      return <li key={key} className={`already-guessed ${correct}`}>{player.handle}</li>
     } else {
-      return <li key={key}>{player.handle} 
-        <span>(not submitted)</span>
-        <button onClick={() => props.skipPlayer(player._id)}>{"<--"} Skip them</button>
+      return <li key={key}>{player.handle}
+        <span> (not submitted)</span>
+        <button onClick={() => props.skipPlayer(player._id)} className='skip-button'>Skip</button>
       </li>
     }
   });
 
   return (
-    <div>
+    <div className='header-submission-list'>
       <h3>{props.teamData.teamName}</h3>
       <ul>
         {playersList}
