69b282e1c8b0157934de367bd4305bee98df8ad2
MorganHuegel <mhue2910@gmail.com>
2019-03-08 15:19:36 -0500

    styled teacher landing page

--- src/teacherProfile/teacherDashboard/createNewButton.js
+++ src/teacherProfile/teacherDashboard/createNewButton.js
@@ -4,7 +4,7 @@ import '../../stylesheets/teacherProfile/teacherDashboard/createNewButton.css';
 
 export function CreateNewButton (props) {
   return (
-    <div className='create-new'>
+    <div className='create-new-button'>
       <button type='button' onClick={() => props.setCreatingState(true)}>
         <i className='fas fa-plus-circle'></i>
         <p>Create New</p>
--- src/teacherProfile/teacherDashboard/gameListItem.js
+++ src/teacherProfile/teacherDashboard/gameListItem.js
@@ -5,16 +5,19 @@ import '../../stylesheets/teacherProfile/teacherDashboard/gameListItem.css';
 export function GameListItem (props) {
   const numOfQuestionsLabel = props.numOfQuestions === 1 ? `${props.numOfQuestions} Question` : `${props.numOfQuestions} Questions`;
   return (
-    <div className='game-list-item'>
-      <h3>{props.title}</h3>
-      <p>{numOfQuestionsLabel}</p>
-      <div>
-        <button type='button' onClick={() => props.clickPlayLive(props.id)}>Play Live</button>
-        <button type='button' onClick={() => props.onEditClick(props.id)}>Edit</button>
-        <button type='button' onClick={() => props.toggleDeletingState(props.id)}>Delete</button>
+    <li className='game-list-item'>
+      <div className='title-container'>
+        <h3>{props.title}</h3>
+        <div className='game-list-buttons'>
+          <button type='button' onClick={() => props.clickPlayLive(props.id)}>Play Live</button>
+          <button type='button' onClick={() => props.onEditClick(props.id)}>Edit</button>
+          <button type='button' onClick={() => props.toggleDeletingState(props.id)}>Delete</button>
+        </div>
       </div>
+      
+      <p>{numOfQuestionsLabel}</p>
 
-      <p>{props.description}</p>
-    </div>
+      <p className='description'>{props.description}</p>
+    </li>
   );
 }
