35dcf59f0370eacaf9dd75d309ffe8e8c7fda8fb
MorganHuegel <mhue2910@gmail.com>
2019-03-19 14:05:55 -0400

    styled Live Game Scoreboard component

--- src/gameplay/teacherView/teamScoreItem.js
+++ src/gameplay/teacherView/teamScoreItem.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import '../../stylesheets/gameplay/teacherView/teamScoreItem.css';
+
 export function TeamScoreItem (props) {
   return (
-    <li>
-      <span>{props.index + 1}</span>
-      <h3>{props.team.teamName}</h3>
-      <p>{props.team.points} Points</p>
-      <p>{props.team.currentQuestion} Questions Answered</p>
+    <li className='team-score-item'>
+      <span className='team-place-number'>{props.index + 1}</span>
+      <h3>
+        {props.team.teamName}
+      </h3>
+      <p className='team-points'>{props.team.points} Points</p>
+      <p className='team-questions-answered'>{props.team.currentQuestion} Questions Answered</p>
     </li>
   )
 }
