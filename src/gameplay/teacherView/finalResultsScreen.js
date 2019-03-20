8aac6373f69fe1c10bbba70ca91a64b25ff9c607
MorganHuegel <mhue2910@gmail.com>
2019-03-20 11:52:30 -0400

    styled Final Results screens

--- src/gameplay/teacherView/finalResultsScreen.js
+++ src/gameplay/teacherView/finalResultsScreen.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
+import '../../stylesheets/gameplay/teacherView/finalResultsScreen.css';
+
 export function FinalResultsScreen (props) {
   props.gameSession.teamList.sort((teamA, teamB) => teamB.points - teamA.points);
 
   const podium = props.gameSession.teamList.map((team, index) => {
     return (
-      <li key={team._id}>
-        <span>{index + 1}</span>
-        <h3>{team.teamName}</h3>
-        <p>{team.points} Points</p>
+      <li key={team._id} className='podium-team'>
+        <span className='team-place'>{index + 1}</span>
+        <h3 className='team-name'>{team.teamName}</h3>
+        <p className='team-points'>{team.points} Points</p>
       </li>
     )
   })
 
   return (
-    <div>
+    <div className='final-results-screen'>
       <h2>Congratulations</h2>
       <button type='button' onClick={() => console.log('Restart clicked')}>Restart Game</button>
-      <ul>
+      <ul className='podium-team-list'>
         {podium}
       </ul>
     </div>
