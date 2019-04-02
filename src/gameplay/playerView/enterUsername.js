611d7d2c85a8f0f6af8056155b7b8e82e1a8a4f3
MorganHuegel <mhue2910@gmail.com>
2019-04-02 11:53:28 -0400

    updated background styles for Enter Username form

--- src/gameplay/playerView/enterUsername.js
+++ src/gameplay/playerView/enterUsername.js
@@ -6,7 +6,7 @@ export function EnterUsername(props){
   return (
     <div className='enter-username'>
         <p>Game Session: {props.sessionCode}</p>
-        <form name='enter-username-form' onSubmit={e => props.onUsernameSubmit(e)}>
+        <form name='enter-username-form' id='enter-username-form' onSubmit={e => props.onUsernameSubmit(e)}>
           <label htmlFor='enter-name'>Enter your name:</label>
           <input type='text' id='enter-name' name='enter-name' placeholder='e.g. Chris P.'/>
           <button type='submit'>Enter</button>
