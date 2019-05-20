92803969920cd2e1742e503face43452e7bb5a19
MorganHuegel <mhue2910@gmail.com>
2019-05-20 12:49:31 -0400

    added validation for individual answers as teacher creates questions

--- src/teacherProfile/teacherGameCreation/createQuestions/newEquationInput.js
+++ src/teacherProfile/teacherGameCreation/createQuestions/newEquationInput.js
@@ -2,32 +2,74 @@ import React from 'react';
 
 import '../../../stylesheets/teacherProfile/teacherGameCreation/newEquationInput.css';
 
-export function NewEquationInput (props) {
-  const equationValue = (props.equation || props.equation === 0) ? props.equation : '';
-  const answerValue = (props.answer || props.answer === 0) ? props.answer : '';
-  
-  return (
-    <fieldset className='new-equation-input'>
-      <legend>Question {props.equationNum}</legend>
+import { valdiateAnswerOnBlur } from './validateCreateQuestions';
 
-      <label htmlFor={`equation${props.equationNum}`}>Equation:</label>
-      <input 
-        defaultValue={equationValue}
-        type='text' 
-        name={`equation${props.equationNum}`} 
-        id={`equation${props.equationNum}`}
-        placeholder={'e.g. 2x + 1 = 7'}
-        className='create-equation-input'
-        />
+export class NewEquationInput extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      validating: false,
+      correct: false,
+      incorrect: false,
+      couldNotVerify: false,
+      message: ''
+    }
+  }
 
-      <label htmlFor={`answer${props.equationNum}`}>Answer:</label>
-      <input 
-        defaultValue={answerValue}
-        type='number'
-        name={`answer${props.equationNum}`} 
-        id={`answer${props.equationNum}`}
-        className='create-answer-input'
-        />
-    </fieldset>
-  )
+  render () {
+    const equationValue = (this.props.equation || this.props.equation === 0) ? this.props.equation : '';
+    const answerValue = (this.props.answer || this.props.answer === 0) ? this.props.answer : '';
+    
+    let classToApply, verifyIcon;
+    switch(true){
+      case(this.state.validating):
+        classToApply = '';
+        verifyIcon = '...validating...';
+        break;
+      case (this.state.correct):
+        classToApply = 'correct';
+        verifyIcon = '...correct...';
+        break;
+      case (this.state.incorrect):
+        classToApply = 'incorrect';
+        verifyIcon = '...incorrect...';
+        break;
+      case (this.state.couldNotVerify):
+        classToApply = 'couldNotVerify';
+        verifyIcon = '...couldNotVerify...';
+        break;
+      default:
+        classToApply = '';
+        verifyIcon = '';
+    }
+
+    return (
+      <fieldset className={`new-equation-input ${classToApply}`}>
+        <legend>Question {this.props.equationNum}<span className='verify-icon'>{verifyIcon}</span></legend>
+
+        <label htmlFor={`equation${this.props.equationNum}`}>Equation:</label>
+        <input 
+          onBlur={event => valdiateAnswerOnBlur(event, this)}
+          defaultValue={equationValue}
+          type='text' 
+          name={`equation${this.props.equationNum}`} 
+          id={`equation${this.props.equationNum}`}
+          placeholder={'e.g. 2x + 1 = 7'}
+          className='create-equation-input'
+          />
+
+        <label htmlFor={`answer${this.props.equationNum}`}>Answer:</label>
+        <input 
+          onBlur={event => valdiateAnswerOnBlur(event, this)}
+          defaultValue={answerValue}
+          type='number'
+          name={`answer${this.props.equationNum}`} 
+          id={`answer${this.props.equationNum}`}
+          className='create-answer-input'
+          />
+
+          <p className='error-message'>{this.state.message}</p>
+      </fieldset>
+    )
+  }
 }
--- src/teacherProfile/teacherGameCreation/createQuestions/validateCreateQuestions.js
+++ src/teacherProfile/teacherGameCreation/createQuestions/validateCreateQuestions.js
@@ -1,3 +1,5 @@
+import { validateCorrectAnswer } from '../../../fetchFunctions/teachers/validateCorrectAnswer';
+
 export function validateCreateQuestions () {
   const equationInputs = [...document.getElementsByClassName('create-equation-input')];
   const answerInputs = [...document.getElementsByClassName('create-answer-input')];
@@ -44,5 +46,66 @@ export function validateCreateQuestions () {
       answer: Number(document.getElementById('answer4').value)
     }
   }
+}
+
+
+
+export function valdiateAnswerOnBlur (event, component) {
+  let question, answer;
+  const input = event.target;
+  const inputId = input.id;
+  const equationNumber = inputId.slice(-1);
+
+  if (inputId.startsWith('equation')) {
+    question = input.value;
+    answer = document.getElementById(`answer${equationNumber}`).value;
+  } else {
+    answer = input.value;
+    question = document.getElementById(`equation${equationNumber}`).value
+  }
+
+  if (!question || !answer) return;
+  component.setState({
+    validating: true,
+    correct: false,
+    incorrect: false,
+    couldNotVerify: false,
+    message: ''
+  }, () => {
+    return validateCorrectAnswer({question, answer})
+      .then(response => {
+        if (response.correct && !response.incorrect) {
+          component.setState({
+            validating: false,
+            correct: true,
+            incorrect: false,
+            couldNotVerify: false,
+            message: ''
+          })
+          //parent.classList.add('correct');
+          //parent.classList.remove('incorrect', 'couldNotVerify')
+        } else if (response.incorrect) {
+          component.setState({
+            validating: false,
+            correct: false,
+            incorrect: true,
+            couldNotVerify: false,
+            message: response.message
+          })
+          //parent.classList.add('incorrect');
+          //parent.classList.remove('correct', 'couldNotVerify');
+        } else {
+          component.setState({
+            validating: false,
+            correct: false,
+            incorrect: false,
+            couldNotVerify: true,
+            message: response.message
+          })
+          //parent.classList.add('couldNotVerify');
+          //parent.classList.remove('correct', 'incorrect');
+        }
+      })
+  })
 
 }
